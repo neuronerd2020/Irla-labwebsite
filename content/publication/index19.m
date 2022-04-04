66b15641bb889177f22c9af3e8ad7311e3d35be7
neuronerd2020 <89407908+neuronerd2020@users.noreply.github.com>
2022-04-04 19:53:56 -0400

    Create index19.m

---
+++ content/publication/index19.m
@@ -0,0 +1,35 @@
+---
+title: "v-SNARE composition distinguishes synaptic vesicle pools"
+authors:
+- Zhaolin Hua
+- Sergio Leal-Ortiz
+- Sarah M Foss
+- Clarissa L Waites
+- Craig C Garner
+- Susan M Voglmaier
+- Robert H Edwards
+date: "2011-08-11T00:00:00Z"
+doi: "10.1016/j.neuron.2011.06.010"
+
+# Schedule page publish date (NOT publication's date).
+publishDate: "2017-01-01T00:00:00Z"
+
+# Publication type.
+# Legend: 0 = Uncategorized; 1 = Conference paper; 2 = Journal article;
+# 3 = Preprint / Working Paper; 4 = Report; 5 = Book; 6 = Book section;
+# 7 = Thesis; 8 = Patent
+publication_types: ["2"]
+
+# Publication name and optional abbreviated publication name.
+publication: In 
+publication_short: 
+
+
+# Summary. An optional shortened abstract.
+
+
+links:
+- name: Article Link
+  url: https://pubmed.ncbi.nlm.nih.gov/21835344/
+  
+---
